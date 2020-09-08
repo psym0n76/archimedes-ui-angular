319260085b78a85886deb0b9c0339cac7bf092aa
Simon Burton <burt76@outlook.com>
2020-09-08 22:52:23 +0100

    toaster properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,11 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   providers: [
     {
