ff7ee0a5164039ce2e38823d11e90457dbe3a624
Simon Burton <burt76@outlook.com>
2020-10-26 12:58:17 +0000

    f

--- src/app/components/fetch-market-data-grid/fetch-market-data-grid.component.ts
+++ src/app/components/fetch-market-data-grid/fetch-market-data-grid.component.ts
@@ -76,6 +76,7 @@ ngOnInit(): void {
   this.hubConnection.on('Update',
           (type: Market) => {
               this.agGrid.api.forEachNode((rowNode, index): any => {
+                console.log(rowNode.data.name + ' ' + type.name + ' ' + rowNode.data.granularity + ' ' + type.timeFrameInterval);
                 if (rowNode.data.name === type.name && rowNode.data.granularity === type.timeFrameInterval) {
                   console.log('Update receieved ' + type.lastUpdated  + ' to replace ' + rowNode.data.lastUpdated);
                   rowNode.data.minDate = type.minDate;
